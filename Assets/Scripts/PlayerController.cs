74ab5b3e9878b78e0a2dbd2ea55134807cfdb510
원하진 <wonhj12@naver.com>
2021-01-14 11:45:49 +0900

    Added Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;         // 플레이어 이동 속도
+    public float jumpForce;     // 플레이어 점프
+
+    private float moveInput;    // 좌우 방향키 인풋
+    private bool facingRight = true;    // 플레이어가 바라보고 있는 방향
+
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rigidBody.velocity = new Vector2(moveInput * speed, rigidBody.velocity.y);  // 플레이어 이동
+
+        // 플레이어 바라보는 방향 설정
+        if (!facingRight && moveInput > 0)
+            Flip();
+        else if (facingRight && moveInput < 0)
+            Flip();
+    }
+
+    // 플레이어 바라보는 방향 설정
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+}
