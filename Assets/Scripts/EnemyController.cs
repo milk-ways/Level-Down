f2c883571406dbfd4be7717ef74556967f5ad247
원하진 <wonhj12@naver.com>
2021-01-16 15:22:57 +0900

    Fixed Dash and added player attack

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public void TakeDamage(int damage)
+    {
+        // Recieve damage
+        Debug.Log("Recieved damage");
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,33 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Player basic settings
+    [Header("Basic Settings")]
+    public int hp;
+    
     // Player movement related
     [Header("Movement")]
-    public float speed;         // Move speed
-    float moveInput;            // Horizontal input
+    public float speed;                             // Move speed
+    float moveInput;                                // Horizontal input
     [SerializeField] int faceDir = 1;            // Player facing direction 1:right, -1:left
 
     // Player dash related
     [Header("Dash")]
-    public float dashSpeed;             // dash speed
-    public float defaultDashTime;       // dash time
-    [SerializeField] float dashTime;    // dash timer
-    [SerializeField] bool dash = false;                  // true:dash, false:stop dash
-
+    public float dashSpeed;                           // dash speed
+    public float defaultDashTime;                  // dash lasting time
+    [SerializeField] float dashTime;                 // dash lasting countdown timer
+    [SerializeField] bool dash = false;             // true:dash, false:stop dash
 
     // Player Jump related
     [Header("Jump")]
-    public float jumpForce; // Jumping force
-    float verticalInput;    // Vertical input
+    public float jumpForce;                      // Jumping force
+    float verticalInput;                             // Vertical input
     [SerializeField] int extraJumps = 1;     // Number of jumps
 
     // Ground Check related
     [Header("Ground Check")]
-    public Transform groundCheck;   // Ground check position
-    public LayerMask groundLayer;   // Ground layer
-    public float groundRadius;      // Ground check radius
+    public Transform groundCheck;                  // Ground check position
+    public LayerMask groundLayer;                  // Ground layer
+    public float groundRadius;                         // Ground check radius
     [SerializeField] bool isGrounded;                // True if player on ground
-    
+
+    // Attack Related
+    [Header("Attack")]
+    public Transform meleeAtkPos;               // Melee attack position
+    public LayerMask enemyLayer;                // Enemy layer
+    public float meleeAttackRange;                         // Melee attack range (radius)
+    public int attackDamage;                        // Attack damage
+
     Rigidbody2D rigidBody;
 
     void Start()
@@ -44,6 +54,7 @@ public class PlayerController : MonoBehaviour
         // Ground check
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
 
+        // Movement
         moveInput = Input.GetAxisRaw("Horizontal");
         rigidBody.velocity = new Vector2(moveInput * speed, rigidBody.velocity.y);  // Move
 
@@ -53,25 +64,19 @@ public class PlayerController : MonoBehaviour
         else if (faceDir == 1 && moveInput < 0)
             Flip();
 
-        if (!dash)
-        {
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                dash = true;
-            }
-        }
-        else
+        // Dash
+        if (dash)
         {
             if (dashTime <= 0)
             {
-                dashTime = defaultDashTime;
-                rigidBody.velocity = Vector2.zero;
                 dash = false;
+                dashTime = defaultDashTime;
+                rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
             }
             else
             {
-                dashTime -= Time.deltaTime;
                 rigidBody.velocity = new Vector2(faceDir * dashSpeed, rigidBody.velocity.y);
+                dashTime -= Time.deltaTime;
             }
         }
     }
@@ -90,9 +95,26 @@ public class PlayerController : MonoBehaviour
                 rigidBody.velocity = Vector2.up * jumpForce;    // Jump
                 extraJumps--;
             }
-            else if (verticalInput < 0 && isGrounded)     // Down arrow
+            else if (verticalInput < 0 && isGrounded)     // Down arrow and is grounded
             {
                 Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer).gameObject.GetComponent<PlatformEffector2D>().surfaceArc = 180f;    // move down
+            }
+        }
+
+        // Dash
+        if (!dash && Input.GetKeyDown(KeyCode.D))
+        {
+            dash = true;
+        }
+      
+
+        // Melee Attack
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Collider2D[] enemies = Physics2D.OverlapCircleAll(meleeAtkPos.position, meleeAttackRange, enemyLayer);
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i].GetComponent<EnemyController>().TakeDamage(attackDamage);
             }
         }
     }
@@ -106,15 +128,10 @@ public class PlayerController : MonoBehaviour
         transform.localScale = scaler;
     }
 
-    void Dash()
-    {
-
-    }
-
-
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);      // Ground check gizmo
+        Gizmos.DrawWireSphere(meleeAtkPos.position, meleeAttackRange);      // Melee attack gizmo
     }
 }
