d5f18adb78ec116a5d4ca741d4d17be82abb5d7f
jwleekhan <jwleekhan@korea.ac.kr>
2021-01-18 23:56:45 +0900

    Enemy HP, get damage

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,9 +4,50 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    // Player basic settings
+    [Header("Basic Settings")]
+    public int hp;
+
+    // Player movement related
+    [Header("Movement")]
+    public float speed;                             // Move speed
+
+    [Header("Get Damage")]
+    [SerializeField] bool getDamage;             // true:get damage, false:immortal
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+    void Update()
+    {
+    }
+
     public void TakeDamage(int damage)
     {
-        // Recieve damage
-        Debug.Log("Recieved damage");
+        if (getDamage == true)
+        {
+            // Recieve damage
+            Debug.Log("Recieved damage");
+
+            hp -= damage;
+
+            if (hp <= 0)
+            {
+                Die();
+            }
+        }
+
+        else
+            Debug.Log("Did not Recieve damage");
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }
