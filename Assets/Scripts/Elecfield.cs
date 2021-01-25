e6a5b4683c441bf7a313454c9a3ec8d1f0a84b8d
jwleekhan <jwleekhan@korea.ac.kr>
2021-01-25 16:03:22 +0900

    Elecfield

---
+++ Assets/Scripts/Elecfield.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elecfield : MonoBehaviour
+{
+    void Start()
+    {
+        InvokeRepeating("SetActive", 1, 1);
+    }
+
+    void SetActive()
+    {
+        if (gameObject.activeSelf)
+            gameObject.SetActive(false);
+        else
+            gameObject.SetActive(true);
+    }
+}
