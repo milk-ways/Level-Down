cca2529de31287ca1384ba7da66b24cdc2b3e84c
dlrudvy0303 <64008277+dlrudvy0303@users.noreply.github.com>
2021-02-07 15:22:34 +0900

    메인,설정화면 UI (미완)

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject settingPanel;
+    public GameObject menuPanel;
+
+    public void LoadGame()
+    {
+        menuPanel.SetActive(false);
+    }
+
+    public void NewGame()
+    {
+        menuPanel.SetActive(false);
+    }
+    public void Setting()
+    {
+        settingPanel.SetActive(true);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    
+}
---
+++ Assets/Scripts/SettingController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingController : MonoBehaviour
+{
+    public GameObject settingPanel;
+
+    private void Start()
+    {
+        settingPanel.SetActive(false);
+    }
+    public void exitSetting()
+    {
+        settingPanel.SetActive(false);
+    }
+}
+
