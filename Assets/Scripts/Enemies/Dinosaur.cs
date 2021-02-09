870f3a9afec6b59d5a4767f664fdd0d367460f71
jwleekhan <jwleekhan@korea.ac.kr>
2021-02-09 17:24:25 +0900

    공룡 복구

---
+++ Assets/Scripts/Enemies/Dinosaur.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dinosaur : EnemyController
+{
+    public int moveSec = 0;
+    public int moveDir = 0;
+
+    void Start()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Debug.Log("Move");
+
+        moveSec = Random.Range(1, 4);
+        Debug.Log(moveSec + "초");
+
+        int newMoveDir = Random.Range(-1, 2);
+        while (moveDir == newMoveDir)
+        {
+            newMoveDir = Random.Range(-1, 2);
+        }
+        moveDir = newMoveDir;
+        Debug.Log(moveDir + "방향");
+
+        if (moveDir == 1)
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        else if (moveDir == -1)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+        Invoke("Move", moveSec);
+    }
+
+    void Update()
+    {
+        rigidBody.velocity = new Vector2(moveDir, 0) * speed;
+    }
+}
