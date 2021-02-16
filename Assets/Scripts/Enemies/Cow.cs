4c3d309c2ba2d254592f3d0217ff3547f03cde09
원하진 <wonhj12@naver.com>
2021-02-16 19:59:23 +0900

    Cow added

---
+++ Assets/Scripts/Enemies/Cow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow : EnemyController
+{
+    [Header("Movement")]
+    public float normalSpeed;
+    public float madSpeed;
+    float speed;
+
+    [Header("Attack")]
+    public LayerMask playerLayer;
+    public Transform sightPos;
+    public float sightRadius;
+    public float returnNormalTime;          // Time for turning back to normal when player out of sight
+    float returnNormalTimer;                // Timer
+    bool isMad = false;                     // Mad state when player in sight
+
+    void Update()
+    {
+        isMad = Physics2D.OverlapCircle(sightPos.position, sightRadius, playerLayer);
+    }
+}
