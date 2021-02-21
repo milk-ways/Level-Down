0611481d6ebf6608711b68e9d7ac44d42513cea9
원하진 <wonhj12@naver.com>
2021-02-21 15:11:11 +0900

    Added BG Music

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    #region Singleton
+    public static MusicController instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this);
+    }
+    #endregion
+}
