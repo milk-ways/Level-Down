5e8f9502ce3f0e046ceb7ba36b6a8dc406e226eb
원하진 <wonhj12@naver.com>
2021-02-22 11:38:35 +0900

    Added boss pattern1

---
+++ Assets/Scripts/Enemies/BossController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : EnemyController
+{
+    [Header("Movement")]
+    public float speed;
+
+    [Header("Attacks")]
+    public LayerMask playerLayer;
+    public Transform attackPos;
+    public float attackRadius;
+    public float patternTime;           // Delay time between patterns
+    bool playerInAtkRange = false;      // Check if player is inside attack range
+
+    public float pattern1DelayTime;
+
+    // Components
+    Rigidbody2D rb;
+    PlayerController player;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        playerInAtkRange = Physics2D.OverlapCircle(attackPos.position, attackRadius, playerLayer);
+
+        
+    }
+
+    void StartPattern()
+    {
+        int rand = Random.Range(1, 101);
+    }
+
+    IEnumerator Pattern1()
+    {
+        yield return new WaitForSeconds(pattern1DelayTime);
+        if (playerInAtkRange)
+        {
+            player.TakeDamage(1);
+        }
+    }
+}
