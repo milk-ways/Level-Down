d7af1b63b3df574b9d16fd904cb3a425370abc00
원하진 <wonhj12@naver.com>
2021-02-22 18:03:10 +0900

    Boss Pattern 2

---
+++ Assets/Scripts/Enemies/Boss/BossPattern2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPattern2 : MonoBehaviour
+{
+    public BossSkill[] bossSkills;
+    public float shootDelayTime;
+    int shootIndex = 0;
+
+    private void Start()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    IEnumerator Shoot()
+    {
+        yield return new WaitForSeconds(shootDelayTime);
+        bossSkills[shootIndex].Shoot();
+        shootIndex++;
+        if (shootIndex < bossSkills.Length)
+            StartCoroutine(Shoot());            // Repeat until every skills are shot
+    }
+
+}
