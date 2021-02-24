ccb001f8896cb7444e7c73fcee4802e9b7d0751e
jwleekhan <jwleekhan@korea.ac.kr>
2021-02-24 18:32:58 +0900

    박쥐

--- Assets/Scripts/Enemies/Bat.cs
+++ Assets/Scripts/Enemies/Bat.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class Bat : EnemyController
 {
+    public float speed;
 
+    GameObject player;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
+        speed = 0;
+        Invoke("Move", 0.5f);
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = (player.transform.position - transform.position).normalized * speed;
+    }
+
+    void Move()
+    {
+        speed = 6;
+    }
 }
