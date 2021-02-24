7f717ee8d9e7b50c37f7fbbf5b38650efecd447b
dlrudvy0303 <64008277+dlrudvy0303@users.noreply.github.com>
2021-02-25 04:01:34 +0900

    슬라임 분할

--- Assets/Scripts/Enemies/Slime.cs
+++ Assets/Scripts/Enemies/Slime.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class Slime : EnemyController
 {
     [Header("Setting")]
-    SightController sight;
+    SightController sight;    
+    [SerializeField] Rigidbody2D slime_1;
+    [SerializeField] Rigidbody2D slime_2;
+    public int maxHp;
     public float sightDistance;
     public float speed;
     public float jumpForce;
+    public float divideForce;
     public LayerMask layer;
 
     [Header("State")]
     public bool isMad;
     public bool canJump;
     public bool isJumping;
-    
+
     Rigidbody2D slimeRigid;
     GameObject player;
     Animator anim;
 
+    
+    
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -81,5 +87,44 @@ public class Slime : EnemyController
         yield return new WaitForSeconds(0.3f);
         isJumping = true;
         slimeRigid.velocity = Vector2.up * jumpForce;
-    }   
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        if (getDamage)
+        {
+            // Recieve damage
+            Debug.Log(gameObject.transform.name + " Recieved damage");
+
+            hp -= damage;
+            Camera.main.GetComponent<CamShake>().BigRand();
+
+            if (hp <= 0)
+            {
+                SlimeDivison();
+            }
+        }
+
+        else
+            Debug.Log("Did not Recieve damage");
+    }
+    void SlimeDivison()
+    {
+        Debug.Log("Hi");
+        if (maxHp == 4)
+        {            
+            Rigidbody2D slime_pre1= Instantiate(slime_1, gameObject.transform.position + new Vector3(0.5f,0,0), Quaternion.Euler(0, 0, 0));
+            Rigidbody2D slime_pre2 = Instantiate(slime_1, gameObject.transform.position - new Vector3(0.5f, 0, 0), Quaternion.Euler(0, 0, 0));
+        }
+        if(maxHp == 2)
+        {
+            Rigidbody2D slime_pre1 = Instantiate(slime_2, gameObject.transform.position + new Vector3(0.5f, 0, 0), Quaternion.Euler(0, 0, 0));
+            Rigidbody2D slime_pre2 = Instantiate(slime_2, gameObject.transform.position - new Vector3(0.5f, 0, 0), Quaternion.Euler(0, 0, 0));
+        }
+        if (deathParticle != null)
+            Instantiate(deathParticle, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+
 }
