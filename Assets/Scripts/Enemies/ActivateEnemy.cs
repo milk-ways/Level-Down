557b7ac1b4cfb930d68701d25cc4c1d2db73426d
원하진 <wonhj12@naver.com>
2021-02-25 17:46:09 +0900

    Enemy Activate Script added

---
+++ Assets/Scripts/Enemies/ActivateEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateEnemy : MonoBehaviour
+{
+    public LayerMask playerLayer;
+    public float activateRadius;
+
+    EnemyController enemy;
+
+    void Start()
+    {
+        enemy = GetComponent<EnemyController>();
+    }
+
+    void Update()
+    {
+        if (Physics2D.OverlapCircle(transform.position, activateRadius, playerLayer))
+            enemy.enabled = true;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, activateRadius);
+    }
+}
