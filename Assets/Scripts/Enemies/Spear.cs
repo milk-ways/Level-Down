b74a6a03afec55ff37af79e2a92461ebae56fe46
dlrudvy0303 <64008277+dlrudvy0303@users.noreply.github.com>
2021-02-27 11:32:14 +0900

    창지기 임시

---
+++ Assets/Scripts/Enemies/Spear.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spear : MonoBehaviour
+{
+    public int spearDamage;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerController>().TakeDamage(spearDamage);
+        }
+    }
+}
--- Assets/Scripts/Enemies/Spearmoai.cs
+++ Assets/Scripts/Enemies/Spearmoai.cs
@@ -10,26 +10,33 @@ public class Spearmoai : EnemyController
     [SerializeField] GameObject spear;
     GameObject player;
 
+    public bool checkDir;
+
 
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        sight = GetComponent<SightController>();
         anim = GetComponent<Animator>();
         spear.transform.localRotation = Quaternion.Euler(0, 0, 90);
     }
     private void Update()
     {
-        inSight = sight.PlayerInSight(Vector2.right, 7.5f) || sight.PlayerInSight(-Vector2.right, 7.5f);
+        
+        inSight =Mathf.Abs(player.transform.position.x - transform.position.x) <= 7.5f;
         anim.SetBool("inSight", inSight);
-        if (inSight)
+        if (inSight && checkDir)
         {
-            transform.right = new Vector3(player.transform.position.x - transform.position.x, 0, 0);
-
-            
+            transform.right = new Vector3(player.transform.position.x - transform.position.x, 0, 0);            
         }
     }
-
+    void Check_True()
+    {
+        checkDir = true;
+    }
+    void Check_False()
+    {
+        checkDir = false;
+    }
     
 }
