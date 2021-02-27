2b0a78359a2cfe5d5642fc6513d717b326451314
원하진 <wonhj12@naver.com>
2021-02-27 23:04:56 +0900

    Thwomp start down

--- Assets/Scripts/Enemies/Thwomp.cs
+++ Assets/Scripts/Enemies/Thwomp.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Thwomp : EnemyController
 {
+    public bool startDown;             // True: start falling, False: start going up
+
     Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        if (!startDown)
+        {
+            rb.gravityScale *= -1;
+        }
 
         InvokeRepeating("Move", 1, 1);
     }
